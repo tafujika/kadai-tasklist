a5b97d945d0b2617a8eb82997216ae9aeba96025
ta.fuji <happiness27you@icloud.com>
2022-01-07 23:48:43 +0000

    add pagy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
-        @tasks = Task.all
+        @pagy, @tasks = pagy(Task.all)
     end
     
     def show
