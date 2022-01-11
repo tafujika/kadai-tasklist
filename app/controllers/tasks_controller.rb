573ecf8f9f731d07b45ca774827ff796819853df
ta.fuji <happiness27you@icloud.com>
2022-01-11 20:57:50 +0000

    add login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,28 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    #before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
+    
     def index
-        @pagy, @tasks = pagy(Task.all)
+        @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
     end
     
     def show
     end
     
     def new
-        @task = Task.new
+        if logged_in?
+            @task = current_user.tasks.build  # form_with 用
+            @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+        end
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
 
         if @task.save
             flash[:success] = 'タスク　が正常に作成されました'
-            redirect_to @task
+            redirect_to root_url
         else
             flash.now[:danger] = 'タスク　の作成に失敗しました'
             render :new
@@ -52,5 +58,12 @@ class TasksController < ApplicationController
     # Strong Parameter
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+          redirect_to root_url
+        end
     end
 end
